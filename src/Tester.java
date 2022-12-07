f5221ca91e0ea357f15c680ecd8d38a2fa6e1d8a
Chris <cr1s@hotmail.it>
2022-12-07 22:14:31 +0100

    Add: exercise if 1

---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+    public static void main(String[] args) {
+
+        String myName = "Christian";
+
+        if(myName.length() % 2 == 0){
+            System.out.println("Your name number of letters is Even");
+        }else{
+            System.out.println("Your name number of letters is Odds");
+        }
+    }
+}
